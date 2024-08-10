c288fdcb39f766c9f334f09dffb257cfe19b1fb5
soleil.colza <soleil.colza@icloud.com>
2024-08-10 23:19:45 +0900

    Add deps: Glance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.lottie.compose)
+    implementation(libs.androidx.glance)
+    implementation(libs.androidx.glance.appwidget)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
