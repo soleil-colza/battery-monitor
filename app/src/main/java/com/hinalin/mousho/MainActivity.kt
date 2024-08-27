dd51cb97cacfd947fb72f2fd539af509d9faa52e
soleil.colza <soleil.colza@icloud.com>
2024-08-28 05:31:00 +0900

    remove TopAppBar

--- app/src/main/java/com/hinalin/mousho/MainActivity.kt
+++ app/src/main/java/com/hinalin/mousho/MainActivity.kt
@@ -8,17 +8,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -131,7 +124,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(batteryMonitor: BatteryTemperatureMonitor) {
     MasterScreen(batteryMonitor)
